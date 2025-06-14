d27841d1ee84af3fa40569ee154246dddf7f0d6e
nevil811 <nevilbarick69@gmail.com>
2025-06-14 22:12:53 +0530

    Math

---
+++ JAVASCRIPT/01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400;
+// console.log('Score:', score); // Output: Score: 400
+const balance = new Number(1000.50);
+// console.log( balance); // Output: Balance: 1000.5
+// console.log(balance.toString());
+// console.log(balance.toFixed(2)); // Output: 1000.50
+
+// +++++++++++ Maths +++++++++++
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log('Math.ceil(4.2):', Math.ceil(4.2)); // Output: 5
+// console.log('Math.floor(4.8):', Math.floor(4.8)); // Output: 4
+// console.log('Math.round(4.4):', Math.round(4.5)); // Output: 5
+// console.log('Math.max(10, 20, 30):', Math.max(10, 20, 30)); // Output: 30
+// console.log('Math.min(10, 20, 30):', Math.min(10, 20, 30)); // Output: 10
+// console.log('Math.random():', Math.random()); // Output: Random number between 0 and 1
+
+
+// console.log(Math.random() * 100); // Random number between 0 and 100
+
+console.log((Math.random() * 10)+1 ); // Random number between 0 and 100
