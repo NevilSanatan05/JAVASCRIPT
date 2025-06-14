dbe43470da37df0087040b2c4070deb6a009d8e5
nevil811 <nevilbarick69@gmail.com>
2025-06-14 11:24:09 +0530

    datatypes and ECMA  standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello World"); //we are using console.log instead of alert
+
+console.log(3 + 3);  //code readibility should be maintained
+
+
+console.log(typeof null); // null is an object type in JS, but it represents the absence of value
+console.log(typeof undefined); //undefined is a type in JS
