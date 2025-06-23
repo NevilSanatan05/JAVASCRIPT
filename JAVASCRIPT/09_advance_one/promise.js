e8611bb9e028ef24b2ca7d729c293383afa9fb9e
nevil811 <nevilbarick69@gmail.com>
2025-06-23 11:58:47 +0530

    Promise in javscript

--- JAVASCRIPT/09_advance_one/promise.js
+++ JAVASCRIPT/09_advance_one/promise.js
@@ -62,6 +62,33 @@ promiseFour
     console.log("The Promise is resolved or rejected");
   });
 
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
 
-
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("Error : Js Went Wrong");
+    }
+  }, 1000);
+});
+async function consumePromiseFive() {
+  try{const response = await promiseFive;
+  console.log(response);
+}
+catch(error){
+  console.log(error);
   
+}
+}
+consumePromiseFive();
+
+fetch('https://api.github.com/users/NevilSanatan05')
+.then((response)=>{
+  return response.json()
+})
+.then((data)=>{
+  console.log(data);
+  
+})
