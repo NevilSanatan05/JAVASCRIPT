a0d8c34fc8a43595575a399c9de18228352a07fb
nevil811 <nevilbarick69@gmail.com>
2025-06-25 11:11:53 +0530

    variables, types, conversion, comparison, strings, numbers, dates

---
+++ JAVASCRIPT/01_basics/01_basics_practice/variables.js
@@ -0,0 +1,127 @@
+// var name = "raj";
+// var age = 18;
+// console.log(name);
+
+// console.log("My age is", age);
+
+// var city = "Mumbai";
+// var city = "Delhi";
+// console.log(city);
+
+// const country = "India";
+// country = "USA";
+// console.log(country);
+
+//  console.log(x);
+// var x = 5;
+
+// {
+//   let a = 10;
+//   console.log(a);
+// }
+
+// let score = 100;
+// score = 200;
+// console.log(score);
+
+// const fruits = ["apple", "banana"];
+// fruits.push("mango");
+// console.log(fruits);
+
+//  console.log(typeof fruits);
+// var a = 1;
+// {
+//   var a = 2;
+//   console.log(a);
+// }
+// console.log(a);
+
+// let a = 1;
+
+// {
+//   let a = 2; !
+//   console.log(a);
+// }
+
+// console.log(a);
+// function testVar() {
+//   var x = 10;
+//   if (true) {
+//     var x = 20;
+//     console.log("Inside if block:", x); // 20
+//   }
+//   console.log("Outside if block:", x); // 20
+// }
+
+// testVar();
+// function myFunc() {
+//   var secret = "This is hidden inside the function";
+//   console.log("Inside function:", secret);
+// }
+
+// myFunc();
+
+// console.log("Outside function:", secret);
+
+// let a ;
+// console.log(a);
+// console.log(typeof a);
+
+// let b = null
+// console.log(b);
+// console.log(typeof b);
+
+// let Value = 20;
+// console.log(typeof Value);
+// let Value1 = String(Value);
+// console.log(typeof Value1);
+
+// let score = Number("hitesh");
+// console.log(typeof score);
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log("2" == 2);
+
+// const score = 400;
+// const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance);
+
+//  console.log(balance.toFixed(1));
+
+// console.log(Math);
+// console.log(Math.random());
+// console.log(Math.round(4.7));
+//  console.log(Math.ceil(4.2));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// let my = new Date();
+// console.log(my);                      // full date object
+// console.log(my.toDateString());      // string: "Wed Jun 25 2025"
+// console.log(my.getMonth());          // number: 0–11 (June is 5)
+// console.log(my.getDay());
+
+// console.log(my.toDateString());
+// console.log(my.toLocaleDateString());
+// console.log(typeof my);
+
+// let g = Date.now()
+// console.log(g);
+
+
+
+
+// DataTypes Summary
+
