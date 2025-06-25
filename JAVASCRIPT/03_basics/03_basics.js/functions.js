702a532ccd47293730c238ad2aa1fd4287ff3313
nevil811 <nevilbarick69@gmail.com>
2025-06-25 12:21:38 +0530

    Functions and its scopes

---
+++ JAVASCRIPT/03_basics/03_basics.js/functions.js
@@ -0,0 +1,41 @@
+// function add(num1, num2) {
+//   // console.log(num1 + num2);
+//   return num1 + num2;
+// }
+// console.log(add(3, 5));
+
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
+// greet("Raj");
+// Immediately Invoked Function Expressions (IIFE)
+
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh')
+
+
+// greet("Nevil");
+
+// function greet(name) {
+//   console.log("Hello " + name);
+// }
+// const user = {
+//   username: "raj",
+//   sayHi: () => {
+//     console.log(`Hi, ${this.username}`);
+//   }
+// };
+// user.sayHi();
+
+
+const greet = (name) => {
+console.log(`hii ${name}`);
+
+}
+greet("rak")
