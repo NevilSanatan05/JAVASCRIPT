97b6e8fe35cd9acf6a48e61611ada764cf520c90
nevil811 <nevilbarick69@gmail.com>
2025-07-06 13:23:14 +0530

    Interview

---
+++ JAVASCRIPT/INTERVIEW/script.js
@@ -0,0 +1,39 @@
+//console.log("A" - 2); // Output: NaN
+//console.log('Nevil' + "5"); // Output: "Nevil05"
+//console.log(2+"2"+"2"); // Output: "222"
+//console.log(2+"2"-"2"); // Output: 20
+
+//Tricky part of the code
+let a={}
+let b = {
+  name:"Nevil"
+}
+let c = {
+  name:"Anil"
+}
+let d= {
+  name:"Rav"
+}
+a[b] = {
+  name :"Ankit"
+}
+a[c]={
+  name:"Raj"
+}
+a[d] = {
+  name:"Ravi"
+}
+
+//console.log(a[b]);
+
+let y= 0 
+let z=false
+//console.log(y==z) ;  // True
+//console.log(y===z) ; // False 
+
+console.log("NA" - 1); // Output: NaN
+
+
+
+
+
