9600ddba92692f4c395b8cc8fa2da53293f10708
nevil811 <nevilbarick69@gmail.com>
2025-07-19 21:35:34 +0530

    data and varibles

---
+++ JAVASCRIPT/NevSan/variables and data.js
@@ -0,0 +1,11 @@
+// Primitive data types :- number, string, boolean, null, undefined, symbol
+let student = {
+  name:"Nevil",
+  age:18
+}
+//console.log(student);
+
+console.log(typeof student);
+
+student.phone = 1234567890;
+console.log(student);
